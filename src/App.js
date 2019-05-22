e233624c556dff3f1173b06166e128afd43f27c4
arsts <arstsu@gmail.com>
2019-05-22 10:54:00 +0300

    Cleaned folders

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import Header from './components/Header/Header';
-import NewTodo from './components/NewTodo/NewTodo';
-import TodoList from './components/TodoList/TodoList';
-import './App.css';
+import React, { Component } from 'react';
+import Todos from './components/Todos/Todos'
 
-const todos = [
-  {text: 'buy milk'},
-  {text: 'do the laundry'},
-  {text: 'walk the dog'},
-  {text: 'buy milk'},
+export default class App extends Component {
+  state= {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Walk the dog',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Buy milk',
+        completed: false
+      }
+    ]
+  }
 
-];
 
-function App() {
-  return (
-    <div className="App flex-container">
-    <Header />
-    <NewTodo /> 
-    <TodoList />
-    
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <Todos />
+      </div>
+    )
+  }
 }
-
-export default App;
